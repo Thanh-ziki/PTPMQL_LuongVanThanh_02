a39848528bd0d0539d977e11091cd6dd8d88d73b
Thanh-ziki <thanh.luongvan2k@gmail.com>
2023-08-04 10:17:20 +0700

    LuongVanThanh_02

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace LuongVanThanh_02.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
